8382f318e7c66446c78967ba21412e5003688387
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-11 15:31:31 +0200

    add Routes with components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './Header';
 import Entertainment from './Entertainment';
 import Health from './Health';
@@ -11,8 +12,18 @@ import Technology from './Technology';
 class App extends React.Component {
   render() {
     return(
-      <div>
-        <Header />
+      <div className="app-container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Home} />
+            <Route path="/entertainment" component={Entertainment} />
+            <Route path="/health" component={Health} />
+            <Route path="/science" component={Science} />
+            <Route path="/sports" component={Sports} />
+            <Route path="/technology" component={Technology} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   };
