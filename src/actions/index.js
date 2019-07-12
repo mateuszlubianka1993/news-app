0049b1239cc757842a381ab4709defae924b9693
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-12 12:11:58 +0200

    add getTopHeadlines action

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import newsAPI from '../apis/newsAPI';
+
+const newsAPIkey = '7f49acd60acf4796b1d9b3dc9a3ef941';
+
+export const getTopHeadlines = () => async dispatch => {
+    const response = await newsAPI.get(`/top-headlines`, {
+        params: {
+            country: 'us',
+            pageSize: 10,
+            apiKey: newsAPIkey
+        }
+    const result = response.articles;
+
+    dispatch({type: 'GET_TOP_HEADLINES', payload: result })
+};
