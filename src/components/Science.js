41be86a79f791f2fce5713dbf594c07958d384a4
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 17:12:24 +0200

    render news in Science component

--- src/components/Science.js
+++ src/components/Science.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getNews} from '../actions/index';
+import NewsItem from './NewsItem';
 
 class Science extends React.Component {
+
+    componentDidMount() {
+        this.props.getNews('science');
+    };
+
     render() {
+        const {news} = this.props;
+        const renderNews = news.map((news, index) => <NewsItem key={index} news={news} />)
         return(
             <div>
-                Science
+                {renderNews}
             </div>
         );
     };
 }
 
-export default Science;
+const mapStateToProps = (state) => {
+    return {
+        news: state.news
+    }
+};
+
+export default connect(mapStateToProps, {getNews}) (Science);
