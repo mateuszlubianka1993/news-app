5769cce3ba316e577d10151fd0f347d235064c6e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 16:05:11 +0200

    add css to NewsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Container} from 'react-bootstrap';
+import {Card, Container, Button} from 'react-bootstrap';
 import '../styles/NewsItem.css';
 
 class NewsItem extends React.Component {
@@ -8,18 +8,19 @@ class NewsItem extends React.Component {
         return(
             <Container>
                 <Card className="bg-dark text-white card-box">
-                    <Card.Img variant="top" src={news.urlToImage} alt="Card image" />
-                    {/* <Card.ImgOverlay className="mask"> */}
+                    <Card.Img variant="top" src={news.urlToImage} alt="Card image" className="news-img"/>
                         <Card.Title>{news.title}</Card.Title>
                         <Card.Text>
                             {news.description}
                         </Card.Text>
                         <Card.Text>{news.content}</Card.Text>
+                        <Card.Link href={news.url} target="_blank" >
+                            <Button >Full News</Button>
+                        </Card.Link>
                         <footer className="blockquote-footer card-footer">
                             <cite title="Source Title">Published at: {news.publishedAt}</cite>
                             <Card.Text>{news.author}</Card.Text>
                         </footer>
-                    {/* </Card.ImgOverlay> */}
                 </Card>
             </Container>
         );
