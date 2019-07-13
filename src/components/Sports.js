934dfdb4cbfe1e28d7d275c525fdd4caf4fbc9b4
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 17:15:07 +0200

    render news in Sports component

--- src/components/Sports.js
+++ src/components/Sports.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getNews} from '../actions/index';
+import NewsItem from './NewsItem';
 
 class Sports extends React.Component {
+
+    componentDidMount() {
+        this.props.getNews('sports');
+    };
+
     render() {
+        const {news} = this.props;
+        const renderNews = news.map((news, index) => <NewsItem key={index} news={news} />)
         return(
             <div>
-                Sports
+                {renderNews}
             </div>
         );
     };
 }
 
-export default Sports;
+const mapStateToProps = (state) => {
+    return {
+        news: state.news
+    }
+};
+
+export default connect(mapStateToProps, {getNews}) (Sports);
