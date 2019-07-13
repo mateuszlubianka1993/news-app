59ccf8035c8d6a44726f63957a17873d7fa4868d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 17:17:43 +0200

    render news in Technology component

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getNews} from '../actions/index';
+import NewsItem from './NewsItem';
 
 class Technology extends React.Component {
+
+    componentDidMount() {
+        this.props.getNews('technology');
+    };
+
     render() {
+        const {news} = this.props;
+        const renderNews = news.map((news, index) => <NewsItem key={index} news={news} />)
         return(
             <div>
-                Technology
+                {renderNews}
             </div>
         );
     };
 }
 
-export default Technology;
+const mapStateToProps = (state) => {
+    return {
+        news: state.news
+    }
+ };
+
+export default connect(mapStateToProps, {getNews}) (Technology);
