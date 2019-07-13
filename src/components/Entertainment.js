bff0b37e3e5438a6bb15b47106dc3cc38e75b055
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 17:06:42 +0200

    render news in Entertainment component

--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {getNews} from '../actions/index';
+import NewsItem from './NewsItem';
 
 class Entertainment extends React.Component {
+
+    componentDidMount() {
+        this.props.getNews('entertainment');
+    };
+    
     render() {
+        const {news} = this.props;
+        const renderNews = news.map((news, index) => <NewsItem key={index} news={news} />)
         return(
             <div>
-                Entertainment
+                {renderNews}
             </div>
         );
     };
 }
 
-export default Entertainment;
+const mapStateToProps = (state) => {
+    return {
+        news: state.news
+    }
+};
+
+export default connect(mapStateToProps, {getNews}) (Entertainment);
