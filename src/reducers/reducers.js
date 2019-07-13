c541ba55d2af9add95f8f4e47d3642edbd0741c6
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-07-13 16:59:44 +0200

    add newsReducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import topHeadlinesReducer from './topHeadlinesReducer';
+import newsReducer from './newsReducer';
 
 export default combineReducers({
     topHeadlines: topHeadlinesReducer,
+    news: newsReducer
 });
